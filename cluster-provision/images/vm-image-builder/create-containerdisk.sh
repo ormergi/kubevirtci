3b8b9f4291827c9578e1328cbb9f1fa913499e6e
Or Mergi <ormergi@redhat.com>
2020-08-02 22:36:26 +0300

    vm-image-builder: Automate VM customization flow
    Add create-containerdisk.sh
     This script automates the process of customizing
     VM image and build Kubevirt VM container-disk-image
     In following PR this script will be used for creating
     and image automaticly using CI.
    
    Add 'example' and 'fedora-extended' directories
    each containes 'cloud-config' and 'image-url' files.
    
    Running the script will use the example files
    by default.
    
    What this script does:
    - Download the VM image from given URL (VM_IMAGE_URL)
    - Customizing VM image using 'customize-image.sh'
      You can export any image customizing script by
      exporting 'CUSTOMIZE_IMAGE_SCRIPT=<script path>'
    - Building container-disk image using the script
      'build-containerdisk.sh', the image is saved
      in local registry and exported to tar file.
    
    Evironment variables:
    VM_IMAGE_URL:
     URL to download the image
    CLOUD_CONFIG_PATH:
     cloud-conifg file path used  for customizing
     the image with cloud-init
    IMAGE_NAME, TAG:
     container-disk image tag that will be created
     tag details IMAGE_NAME:TAG
    
    Example:
    export IMAGE_NAME="fedora-extended"
    export TAG="32"
    export CLOUD_CONFIG_PATH="my-config"
    ./cleare-push-containerdisk.sh
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

---
+++ cluster-provision/images/vm-image-builder/create-containerdisk.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+SCRIPT_PATH=$(dirname "$(realpath "$0")")
+
+export CUSTOMIZE_IMAGE_SCRIPT=${CUSTOMIZE_IMAGE_SCRIPT:-"${SCRIPT_PATH}/customize-image.sh"}
+
+function customize_image() {
+  local source_image=$1
+  local customized_image=$2
+  local cloud_config=$3
+
+  # Backup the VM image and pass copy of the original image
+  # in case customizing script fail.
+  vm_image_copy="$(dirname $customized_image)/copy-${source_image}"
+  cp "$source_image" "$vm_image_copy"
+
+  # TODO: convert this script and its dependencies to container
+  ${CUSTOMIZE_IMAGE_SCRIPT} "$vm_image_copy" "$customized_image" "$cloud_config"
+
+  # Backup no longer needed.
+  rm -f "$vm_image_copy"
+}
+
+function cleanup() {
+    if [ $? -ne 0 ];then
+      rm -rf "${build_directory}"
+    fi
+
+    rm -f "copy-${VM_IMAGE}"
+}
+
+export IMAGE_NAME=${IMAGE_NAME:-example-fedora}
+export TAG=${TAG:-32}
+export CLOUD_CONFIG_PATH=${CLOUD_CONFIG_PATH:-"${SCRIPT_PATH}/example/cloud-config"}
+export VM_IMAGE_URL=${VM_IMAGE_URL:-$(cat "${SCRIPT_PATH}/example/image-url")}
+
+readonly VM_IMAGE="source-image.qcow2"
+readonly build_directory="${IMAGE_NAME}_build"
+readonly new_vm_image_name="provisioned-image.qcow2"
+
+trap 'cleanup' EXIT SIGINT
+
+pushd "${SCRIPT_PATH}"
+  cleanup
+
+  if ! [ -e "$VM_IMAGE" ]; then
+    # Download base VM image
+    curl -L $VM_IMAGE_URL -o $VM_IMAGE
+  fi
+
+  mkdir "${build_directory}"
+
+  customize_image "$VM_IMAGE" "${build_directory}/${new_vm_image_name}" "${CLOUD_CONFIG_PATH}"
+
+  ${SCRIPT_PATH}/build-containerdisk.sh "${IMAGE_NAME}" "${TAG}" "${build_directory}/${new_vm_image_name}"
+
+popd
