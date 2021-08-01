4a3ec12d935b5cdabd8dcf528fba68d47b529a05
Or Mergi <ormergi@redhat.com>
2021-08-01 14:11:14 +0300

    bind_device_to_vfio.sh: Add support for multiple devices
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- cluster-provision/k8s/1.20/bind_device_to_vfio.sh
+++ cluster-provision/k8s/1.20/bind_device_to_vfio.sh
@@ -2,54 +2,60 @@
 
 set -ex
 
+pci_address_regex="[0-9a-fA-F]{4}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}.\d{1}"
+
 if [ "$1" != "--vendor" ]; then
     echo "No vendor provided"
     exit 1
 fi
 vendor=$2
 
+MDRIVDER="${MDRIVDER:-vfio-pci}"
+
 function get_device_driver() {
     local dev_driver=$(readlink $driver_path)
     echo "${dev_driver##*/}"
 }
 
-# find the PCI address of the device by vendor_id:product_id
-pci_address=(`lspci -D -d ${vendor}`)
-pci_address="${pci_address[0]}"
-dev_sysfs_path="/sys/bus/pci/devices/$pci_address"
-
-if [[ ! -d $dev_sysfs_path ]]; then
-    echo "Error: PCI address ${pci_address} does not exist!" 1>&2
-    exit 1
-fi
-
-if [[ ! -d "$dev_sysfs_path/iommu/" ]]; then
-    echo "Error: No vIOMMU found in the VM" 1>&2
-    exit 1
-fi
-
-# set device driver path
-driver_path="${dev_sysfs_path}/driver"
-driver_override="${dev_sysfs_path}/driver_override"
-
 # load the vfio-pci module
-modprobe -i vfio-pci
+modprobe -i "${MDRIVDER}"
 
+# find the PCI address of the device by vendor_id:product_id
+pci_address=(`lspci -D -d ${vendor} | grep -Po "$pci_address_regex"`)
+for pci_address in "${pci_address[@]}"; do
+    dev_sysfs_path="/sys/bus/pci/devices/$pci_address"
 
-driver=$(get_device_driver)
+    if [ ! -d $dev_sysfs_path ]; then
+        echo "Error: PCI address ${pci_address} does not exist!" 1>&2
+        continue
+    fi
 
-if [[ "$driver" != "vfio-pci" ]]; then
+    if [ ! -d "$dev_sysfs_path/iommu/" -a "$MDRIVDER" == "vfio-pci" ]; then
+        echo "Error: No vIOMMU found in the VM $pci_address" 1>&2
+        continue
+    fi
 
-    # unbind from the original device driver
-    echo ${pci_address} > "${driver_path}/unbind"
-    # bind the device to vfio-pci driver
-    echo "vfio-pci" > ${driver_override}
-    echo $pci_address > /sys/bus/pci/drivers/vfio-pci/bind
-fi
+    # set device driver path
+    driver_path="${dev_sysfs_path}/driver"
+    driver_override="${dev_sysfs_path}/driver_override"
 
-# The device should now be using the vfio-pci driver
-new_driver=$(get_device_driver)
-if [[ $new_driver != "vfio-pci" ]]; then
-    echo "Error: Failed to bind to vfio-pci driver" 1>&2
-    exit 1
-fi
+    driver=$(get_device_driver)
+
+    if [ "$driver" != "${MDRIVDER}" ]; then
+
+        # unbind from the original device driver
+        if [ -f "${driver_path}/unbind" ]; then 
+            echo "${pci_address}" > "${driver_path}/unbind"
+        fi
+        # bind the device to driver
+        echo "${MDRIVDER}" > "${driver_override}"
+        echo "${pci_address}"> "/sys/bus/pci/drivers/${MDRIVDER}/bind"
+    fi
+
+    # The device should now be using the vfio-pci driver
+    new_driver=$(get_device_driver)
+    if [ "$new_driver" != "${MDRIVDER}" ]; then
+        echo "Error: Failed to bind to $MDRIVDER driver $pci_address" 1>&2
+        continue
+    fi
+done
